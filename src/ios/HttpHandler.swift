6558aef2f58ab64ef9737d593e890d896cc25346
Scott Covert <scott.b.covert@gmail.com>
2017-12-05 19:42:27 -0800

    Added HTTPHandler class for sending POST to remote server

---
+++ src/ios/HttpHandler.swift
@@ -0,0 +1,28 @@
+import Foundation
+
+class HTTPHandler {
+    static func postToServer(postURL: String!, postString: String!) {
+        let url = URL(string: postString)!
+        var request = URLRequest(url: url)
+        request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "POST"
+        request.httpBody = postString.data(using: .utf8)
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            // check for fundamental networking error
+            guard let data = data, error == nil else {
+                print("error=\(error)")
+                return
+            }
+
+            // check for http errors
+            if let httpStatus = response as? HTTPURLResponse, httpStatus.statusCode != 200 {
+                print("statusCode should be 200, but is \(httpStatus.statusCode)")
+                print("response = \(response)")
+            }
+
+            let responseString = String(data: data, encoding: .utf8)
+            print("responseString = \(responseString)")
+        }
+        task.resume()
+    }
+}
